3a3e2899a7d8a5ad29e60b7d3247ce20edd38afc
kimdaeyeub <kdy3915@naver.com>
2023-07-24 00:45:50 +0900

    create post screen

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:instagram_clone/constants/gaps.dart';
 import 'package:instagram_clone/constants/sizes.dart';
+import 'package:instagram_clone/features/home/widgets/post.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,53 +11,89 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(
                   horizontal: Sizes.size12,
                 ),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    for (var i in [
-                      1,
-                      1,
-                      1,
-                      11,
-                      1,
-                      1,
-                      11,
-                      1,
-                      1,
-                      1,
-                      1,
-                      1,
-                      1,
-                      1,
-                      2,
-                      1,
-                      1,
-                      1,
-                      11,
-                      1
-                    ]) ...[
-                      const CircleAvatar(
-                        radius: 44,
+                    Text(
+                      "Instagram",
+                      style: TextStyle(
+                        fontFamily: "LogoFont",
+                        fontSize: Sizes.size32,
                       ),
-                      Gaps.h6,
-                    ]
+                    ),
+                    Row(
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size24,
+                        ),
+                        Gaps.h20,
+                        FaIcon(
+                          FontAwesomeIcons.paperPlane,
+                          size: Sizes.size24,
+                        )
+                      ],
+                    )
                   ],
                 ),
               ),
-            ),
-            Gaps.v12,
-            Divider(
-              thickness: 2,
-              color: Colors.grey.shade300,
-            ),
-          ],
+              Gaps.v16,
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: Sizes.size12,
+                  ),
+                  child: Row(
+                    children: [
+                      for (var i in [
+                        1,
+                        1,
+                        1,
+                        11,
+                        1,
+                        1,
+                        11,
+                        1,
+                        1,
+                        1,
+                        1,
+                        1,
+                        1,
+                        1,
+                        2,
+                        1,
+                        1,
+                        1,
+                        11,
+                        1
+                      ]) ...[
+                        const CircleAvatar(
+                          radius: Sizes.size36,
+                        ),
+                        Gaps.h6,
+                      ]
+                    ],
+                  ),
+                ),
+              ),
+              Gaps.v12,
+              Divider(
+                thickness: 2,
+                color: Colors.grey.shade300,
+              ),
+              Gaps.v20,
+              const InstagramPost(),
+              const InstagramPost(),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/features/home/widgets/post.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:instagram_clone/constants/gaps.dart';
+
+import '../../../constants/sizes.dart';
+
+class InstagramPost extends StatelessWidget {
+  const InstagramPost({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size16,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: Sizes.size24,
+                  ),
+                  Gaps.h10,
+                  Text(
+                    "김대엽",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+              FaIcon(FontAwesomeIcons.ellipsis)
+            ],
+          ),
+        ),
+        Gaps.v12,
+        Image.asset(
+          "assets/images/placeholder.jpg",
+          fit: BoxFit.fitWidth,
+        ),
+        Gaps.v20,
+        const Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size16,
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.heart,
+                        size: Sizes.size28,
+                      ),
+                      Gaps.h12,
+                      FaIcon(
+                        FontAwesomeIcons.comment,
+                        size: Sizes.size28,
+                      ),
+                      Gaps.h12,
+                      FaIcon(
+                        FontAwesomeIcons.paperPlane,
+                        size: Sizes.size28,
+                      ),
+                    ],
+                  ),
+                  FaIcon(
+                    FontAwesomeIcons.bookmark,
+                  ),
+                ],
+              ),
+              Gaps.v10,
+              Row(
+                children: [
+                  Text(
+                    "좋아요",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: Sizes.size12 + Sizes.size2,
+                    ),
+                  ),
+                  Gaps.h4,
+                  Text(
+                    "46,025개",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: Sizes.size12 + Sizes.size2,
+                    ),
+                  )
+                ],
+              ),
+              Gaps.v8,
+              Row(
+                children: [
+                  Text(
+                    "kimdaeyeub",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.h8,
+                  Text(
+                    "응아아아악",
+                  )
+                ],
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
